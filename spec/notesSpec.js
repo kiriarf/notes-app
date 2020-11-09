cef354c5e10d069824a8b01cd0dcbab63cb421c7
charlie-galb <galbraith.charlie@gmail.com>
2020-11-09 16:00:43 +0000

    Refactoring: NoteSpec uses IIFE to run self

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -1,6 +1,4 @@
-function noteReturnsTextInput() {
+(function noteReturnsTextInput() {
   var note = new Note("My favourite language is JavaScript");
   assert.isTrue(note.getContent() === "My favourite language is JavaScript");
-}
-
-noteReturnsTextInput();
+})();
