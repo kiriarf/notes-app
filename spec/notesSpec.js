a25812d416ede944e97deea4e07ce5b1ce51f736
charlie-galb <galbraith.charlie@gmail.com>
2020-11-09 15:46:07 +0000

    Refactor test into single function

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -1,9 +1,6 @@
-note = new Note("My favourite language is JavaScript");
-const pass = "pass";
-const fail = "fail";
-
-if (note.getContent() === "My favourite language is JavaScript") {
-  console.log(pass);
-} else {
-  console.log(fail);
+function noteReturnsTextInput() {
+  var note = new Note("My favourite language is JavaScript");
+  assert.isTrue(note.getContent() === "My favourite language is JavaScript");
 }
+
+noteReturnsTextInput();
