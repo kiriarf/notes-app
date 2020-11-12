77dbb3217abf0c8af0686bee23a0d43c05f5410e
Kiril <kiril@olgas-air.mynet>
2020-11-12 09:41:38 +0000

    All notes button now shows all notes, comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+//Setup and references to objects
 let noteController = new NoteController();
 
 const noteInput = document.getElementById("note-input");
 const addNoteBtn = document.getElementById("add-note");
 const noteListElement = document.getElementById("note-list");
 const backBtn = document.getElementById("go-back");
+//----------------------------------------------------------
 
+//Event listener for the add button -> adds a note only if input not blank
 addNoteBtn.addEventListener('click', function() {
   if(noteInput.value === "") {
     alert("A note cannot be blank!");
@@ -15,7 +18,9 @@ addNoteBtn.addEventListener('click', function() {
 
   noteController.insertHTML();
 });
+//---------------------------------------------------------
 
+//For showing a single note on hash change
 makeUrlChangeShowNoteForCurrentPage();
 
 function makeUrlChangeShowNoteForCurrentPage() {
@@ -32,12 +37,32 @@ function getNoteFromUrl(location) {
 
 function showNote(noteId) {
   let noteArr = noteController.noteList.list;
+  //Loops through array of notes and checks if there is a note with the same id as the hash
   for(let i = 0; i < noteArr.length; i++) {
     if(noteId == noteArr[i].id) {
       let singleNoteView = new SingleNoteView(noteArr[i]);
       let noteHTML = singleNoteView.createHTML();
       noteListElement.innerHTML = noteHTML;
+      //Change visibility of elements
       backBtn.style.display = "block";
+      noteInput.style.display = "none";
+      addNoteBtn.style.display = "none";
     };
   };
+};
+// -----------------------------------------------------
+
+//Event listener for the All Notes button to go back to all notes when viewing single note
+backBtn.addEventListener('click', showAllNotes);
+
+function showAllNotes() {
+  //Make All Notes button invisible
+  backBtn.style.display = "none";
+  //This does not trigger onhashchange (which is nice)
+  history.replaceState(null, null, ' ');
+  //Change visibility of elements
+  noteInput.style.display = "inline";
+  addNoteBtn.style.display = "inline";
+  //Show the HTML for all notes in the NoteList
+  noteController.insertHTML();
 };
